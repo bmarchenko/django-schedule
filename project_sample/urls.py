185e0a0f1336679cf394bcc78678f6652e315058
Bogdan Marchenko <bogdan.marko@gmail.com>
2012-07-21 15:29:20 +0300

    deprecated urls

--- project_sample/urls.py
+++ project_sample/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
 
 if settings.DEBUG:
